028774026bc00dc708db916bae7ed8c5a539c31f
NhungLe <hongnhung16052003@gmail.com>
2023-11-25 09:07:59 +0700

    first project

---
+++ src/test/java/com/example/pbl04/Pbl04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pbl04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pbl04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
