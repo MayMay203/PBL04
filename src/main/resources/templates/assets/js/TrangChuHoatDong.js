c674621903c69f906ad70a2b7c8d0808c7ecba66
Bach Duong <bachduongvan0402@gmail.com>
2023-11-29 17:39:29 +0700

    bachduong

---
+++ src/main/resources/templates/assets/js/TrangChuHoatDong.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $('.act-slider').slick({
+    dots:true,
+    slidesToShow:1,
+    arrows:false,
+    vertical:false,
+    speed:100
+  })
+});
+
+$(document).ready(function(){
+  $('.list_card').slick({
+    autoplay:true,
+    infinite:false,
+    slidesToShow:3,
+    vertical:true,
+    prevArrow: '<div class="slick-prev"><i class= "bi bi-chevron-compact-up"></i></div>',
+    nextArrow: '<div class="slick-next"><i class="bi bi-chevron-compact-down"></i></div>',
+  })
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var search_input = document.querySelector(".search-input");
+  var search_info = document.querySelector(".search-info");
+
+  search_input.addEventListener("focus", function (e) {
+      search_info.classList.add('display-block');
+  });
+
+  search_input.addEventListener("blur", function (e) {
+      search_info.classList.remove('display-block');
+  });
+});
+
---
+++ src/main/resources/templates/assets/js/TrangChuTongKet.js
@@ -0,0 +1,14 @@
+var search_input = document.querySelector(".search-input")
+var search_infor = document.querySelector(".search-info")
+search_input.addEventListener("focus", function (e) {
+    search_infor.classList.add('display-block');
+});
+search_input.addEventListener("blur", function (e) {
+    search_infor.classList.remove('display-block');
+});
+
+
+function XemTongKet() {
+    window.location.href = 'TongKetHoatDong.html';
+}
+
