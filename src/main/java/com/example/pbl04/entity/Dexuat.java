06e0de961eb43792c14dd7ae3a484fbb5389a93b
Bach Duong <bachduongvan0402@gmail.com>
2023-12-01 16:34:44 +0700

    demo

---
+++ src/main/java/com/example/pbl04/entity/Dexuat.java
@@ -0,0 +1,79 @@
+package com.example.pbl04.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "dexuat")
+public class Dexuat {
+    @Id
+    @Column(name = "maDX", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "maTK", nullable = false)
+    private Taikhoan maTK;
+
+    @Lob
+    @Column(name = "viTri", nullable = false)
+    private String viTri;
+
+    @Lob
+    @Column(name = "chuDe", nullable = false)
+    private String chuDe;
+
+    @Lob
+    @Column(name = "moTa", nullable = false)
+    private String moTa;
+
+    @Column(name = "trangThai", nullable = false)
+    private Byte trangThai;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Taikhoan getMaTK() {
+        return maTK;
+    }
+
+    public void setMaTK(Taikhoan maTK) {
+        this.maTK = maTK;
+    }
+
+    public String getViTri() {
+        return viTri;
+    }
+
+    public void setViTri(String viTri) {
+        this.viTri = viTri;
+    }
+
+    public String getChuDe() {
+        return chuDe;
+    }
+
+    public void setChuDe(String chuDe) {
+        this.chuDe = chuDe;
+    }
+
+    public String getMoTa() {
+        return moTa;
+    }
+
+    public void setMoTa(String moTa) {
+        this.moTa = moTa;
+    }
+
+    public Byte getTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(Byte trangThai) {
+        this.trangThai = trangThai;
+    }
+
+}
---
+++ src/main/java/com/example/pbl04/entity/Tongket.java
@@ -0,0 +1,57 @@
+package com.example.pbl04.entity;
+
+import com.example.pbl04.entity.Hoatdong;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tongket")
+public class Tongket {
+    @Id
+    @Column(name = "maTongKet", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "maHD", nullable = false)
+    private Hoatdong maHD;
+
+    @Lob
+    @Column(name = "bangTongKet", nullable = false)
+    private String bangTongKet;
+
+    @Lob
+    @Column(name = "loiKet", nullable = false)
+    private String loiKet;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Hoatdong getMaHD() {
+        return maHD;
+    }
+
+    public void setMaHD(Hoatdong maHD) {
+        this.maHD = maHD;
+    }
+
+    public String getBangTongKet() {
+        return bangTongKet;
+    }
+
+    public void setBangTongKet(String bangTongKet) {
+        this.bangTongKet = bangTongKet;
+    }
+
+    public String getLoiKet() {
+        return loiKet;
+    }
+
+    public void setLoiKet(String loiKet) {
+        this.loiKet = loiKet;
+    }
+
+}
