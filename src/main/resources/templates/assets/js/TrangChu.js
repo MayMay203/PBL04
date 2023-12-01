b216a4edde5e26d5d9800211a4e9f0baa7fb8c6e
Panda Nguyễn <ndbaochau104@gmail.com>
2023-12-01 15:22:46 +0700

    tạo file header

--- src/main/resources/templates/assets/js/TrangChu.js
+++ src/main/resources/templates/assets/js/TrangChu.js
@@ -11,3 +11,9 @@ $(document).ready(function(){
     });
 
 });
+
+fetch('../templates/Header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('iframe-container').innerHTML = data;
+});
