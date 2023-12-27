43672cdc4e3c09f115993fe6ce23f80d9058de0c
Bach Duong <bachduongvan0402@gmail.com>
2023-12-27 10:11:17 +0700

    update tongkethoatdong

---
+++ src/main/java/com/example/pbl04/controller/WebSocketController.java
@@ -0,0 +1,14 @@
+// WebSocketController.java
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+//@Controller
+//public class WebSocketController {
+//
+//    @MessageMapping("/hello")
+//    @SendTo("/topic/greetings")
+////    public Greeting greeting(HelloMessage message) throws Exception {
+////        return new Greeting("Hello, " + message.getName() + "!");
+////    }
+//}
